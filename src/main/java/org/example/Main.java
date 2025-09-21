ff0b166d532e2b6e8cf5236f7b3d477c7615b930
Nikita Filippov <nik.filippov05@gmail.com>
2025-09-21 09:11:05 +0300

    add image for readme commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,23 +7,27 @@ public class Main {
     public static void main(String[] args) {
         Map<String, Double> mapa = new LinkedHashMap<>();
         Map<String, Double> mapa2 = new LinkedHashMap<>();
-        System.out.println("result '10+5': " + ExpressionEvaluator.evaluate("10+5", mapa));
-        System.out.println("result '10+-5': " + ExpressionEvaluator.evaluate("10+-5", mapa));
-        System.out.println("result '2.7*(10-3.5)': " + ExpressionEvaluator.evaluate("2.7*(10-3.5)", mapa));
-        //System.out.println("result '2.7*(10-3.5))': " + ExpressionEvaluator.evaluate("2.7*(10-3.5))", mapa));
-        System.out.println("result 'sin(180)': " + ExpressionEvaluator.evaluate("sin(180)", mapa));
-        System.out.println("result 'cos(180)': " + ExpressionEvaluator.evaluate("cos(180)", mapa));
-        System.out.println("result 'sin(90 + sin(180))': " + ExpressionEvaluator.evaluate("sin(90+sin(180))", mapa));
-        System.out.println("result 'sin(90 + sin(90 + sin(90)))': " + ExpressionEvaluator.evaluate("sin(90 + sin(90 + sin(90)))", mapa));
-        System.out.println("result 'cos(90 + cos(90 + cos(90) + 45) + 15*3)': " + ExpressionEvaluator.evaluate("cos(90 + cos(90 + cos(90) + 45) + 15*3)", mapa));
-        System.out.println("result '(10+10/2)': " + ExpressionEvaluator.evaluate("(10+10/2)", mapa));
-        System.out.println("result '((10+10^2))': " + ExpressionEvaluator.evaluate("(10+10^2)", mapa));
-        System.out.println("result '(10+10^2/(sin(90)*2))': " + ExpressionEvaluator.evaluate("(10+10^2/(sin(90)*2))", mapa));
+        System.out.println();
+        System.out.println("10 + 5 = " + ExpressionEvaluator.evaluate("10 + 5", mapa));
+        System.out.println("10 + -5 = " + ExpressionEvaluator.evaluate("10 + -5", mapa));
+        System.out.println("2.7 * (10 - 3.5) = " + ExpressionEvaluator.evaluate("2.7 * (10 - 3.5)", mapa));
+        System.out.println("sin(180) = " + ExpressionEvaluator.evaluate("sin(180)", mapa));
+        System.out.println("cos(180) = " + ExpressionEvaluator.evaluate("cos(180)", mapa));
+        System.out.println("sin(90 + sin(180)) = " + ExpressionEvaluator.evaluate("sin(90 + sin(180))", mapa));
+        System.out.println("sin(90 + sin(90 + sin(90))) = " + ExpressionEvaluator.evaluate("sin(90 + sin(90 + sin(90)))", mapa));
+        System.out.println("cos(90 + cos(90 + cos(90) + 45) + 15*3) = " + ExpressionEvaluator.evaluate("cos(90 + cos(90 + cos(90) + 45) + 15*3)", mapa));
+        System.out.println("(10 + 10/2) = " + ExpressionEvaluator.evaluate("(10 + 10/2)", mapa));
+        System.out.println("((10 + 10^2)) = " + ExpressionEvaluator.evaluate("(10 + 10^2)", mapa));
+        System.out.println("(10 + 10^2/(sin(90) * 2)) = " + ExpressionEvaluator.evaluate("(10 + 10^2/(sin(90) * 2))", mapa));
+        System.out.println("С переменными: ");
         mapa.put("angle", 180.0);
-        System.out.println("result 'cos(angle)': " + ExpressionEvaluator.evaluate("cos(angle)", mapa));
+        System.out.println("angle = 180");
+        System.out.println("cos(angle) = " + ExpressionEvaluator.evaluate("cos(angle)", mapa));
         mapa2.put("angle", 90.0);
         mapa2.put("halfangle", 45.0);
-        System.out.println("result 'cos(angle + cos(angle + cos(angle) + angle/2) + halfangle)': " + ExpressionEvaluator.evaluate("cos(angle + cos(angle + cos(angle) + angle/2) + halfangle)", mapa2));
+        System.out.println("angle = 90");
+        System.out.println("halfangle = 45");
+        System.out.println("cos(angle + cos(angle + cos(angle) + angle/2) + halfangle) = " + ExpressionEvaluator.evaluate("cos(angle + cos(angle + cos(angle) + angle/2) + halfangle)", mapa2));
 
         //System.out.println(Math.cos(Math.toRadians(90 + Math.cos(Math.toRadians(90 + Math.cos(Math.toRadians(90)) + 45)) + 15*3)));
     }
